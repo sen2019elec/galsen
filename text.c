8a8927ea7deca88c8b4edfa104d703d4c2aeb67e
camara <sen.elecplomb2019@gmail.com>
2022-08-28 16:10:26 +0000

    text.1

---
+++ text.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(){
+    int x;
+
+
+    printf("veuillez saisir un nombre ");
+    scanf("%d",&x);
+    if(x>0){
+    printf("le nombre est positive");
+    }
+    else{
+        printf("le nombre est negative");
+    }
+    
+
+
+}
+
